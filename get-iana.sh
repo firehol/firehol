0ae04b1cab9886ca1da72cd484985b70d84411f7
ktsaou <ktsaou>
2002-10-24 21:10:01 +0000

    Removed service TFTP since this requires a kernel module for stateful operation.
    After suggestions by Fco.Felix Belmonte (ffelix@gescosoft.com),
    I have added:
    
    a) RESERVED_IPS, PRIVATE_IPS, MULTICAST_IPS and UNROUTABLE_IPS
    You can use the above in SRC (not) parameters to match them.
    The use of UNROUTABLE_IPS is suggested for cases where an interface is
    exclusivelly public.
    
    b) kernel module requirements per complex service and for the
    configuration file as a whole.
    Now you can use:
    
    # one line for each module, somewhere in your config file
    require_kernel_module <kernel_module>
    
    to have FireHOL require some kernel module to succesfully complete
    the firewall configuration.
    
    As an option for those running NAT, you can use:
    
    FIREHOL_NAT=1 # put this at the top of your config file
    
    to make the complex services require also the NAT modules for the
    services they implement.
    
    Finally, I have added a get-iana.sh script that produces one BASH
    statement for RESERVED_IPS.

---
+++ get-iana.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#
+# Program that downloads the IPv4 address space allocation by IANA
+# and creates a list with all reserved address spaces.
+#
+
+IPV4_ADDRESS_SPACE_URL="http://www.iana.org/assignments/ipv4-address-space"
+IANA_RESERVED="IANA - Reserved"
+
+LOG="/tmp/log.$$"
+
+wget -O - --proxy=off "${IPV4_ADDRESS_SPACE_URL}" 2>>$LOG	|\
+	grep "${IANA_RESERVED}"					|\
+	cut -d ' ' -f 1						|\
+(
+	printf 'IANA_RESERVED="'
+	
+	while IFS="/" read range net
+	do
+		if [ ! $net -eq 8 ]
+		then
+			echo >>$LOG "Cannot handle network masks of $net bits ($range/$net)"
+			continue
+		fi
+		 
+		first=`echo $range | cut -d '-' -f 1`
+		first=`expr $first + 0`
+		last=`echo $range | cut -d '-' -f 2`
+		last=`expr $last + 0`
+		
+		x=$first
+		while [ ! $x -gt $last ]
+		do
+			printf "$x.0.0.0/$net "
+			x=$[x + 1]
+		done
+	done
+	
+	printf '"'
+	echo
+)
+
+echo
+echo "Press enter to view the log..."
+read
+less $LOG
+rm -f $LOG
