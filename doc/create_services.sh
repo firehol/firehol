43f0ae0de07cbb0d6888fb9c21b19bfbdace42b7
ktsaou <ktsaou>
2003-02-11 23:43:28 +0000

    *** empty log message ***

--- doc/create_services.sh
+++ doc/create_services.sh
@@ -626,7 +626,7 @@ all_services |\
 
 
 cat <<"EOF"
-</table>
+</tr></table>
 <hr noshade size=1>
 <p>
 <table border=0 cellspacing=5 cellpadding=10 width="80%">
@@ -652,7 +652,7 @@ cat <<"EOF"
 <tr><td align=center valign=middle>
 	<A href="http://sourceforge.net"><IMG src="http://sourceforge.net/sflogo.php?group_id=58425&amp;type=5" width="210" height="62" border="0" alt="SourceForge Logo"></A>
 </td><td align=center valign=middle>
-	<small>$Id: create_services.sh,v 1.25 2003/02/11 23:41:35 ktsaou Exp $</small>
+	<small>$Id: create_services.sh,v 1.26 2003/02/11 23:43:28 ktsaou Exp $</small>
 	<p>
 	<b>FireHOL</b>, a firewall for humans...<br>
 	&copy; Copyright 2002
