9388014f2b5204a88a5e35bd8b0500a5bcf59fb1
ktsaou <ktsaou>
2003-07-20 21:48:01 +0000

    Changed service CUPS ports to numeric. Gentoo does not have this defined.

--- firehol.sh
+++ firehol.sh
@@ -10,7 +10,7 @@
 #
 # config: /etc/firehol/firehol.conf
 #
-# $Id: firehol.sh,v 1.140 2003/07/20 21:46:41 ktsaou Exp $
+# $Id: firehol.sh,v 1.141 2003/07/20 21:48:01 ktsaou Exp $
 #
 FIREHOL_FILE="${0}"
 
@@ -294,7 +294,7 @@ client_apcupsd_ports="default"
 server_apcupsdnis_ports="tcp/3551"
 client_apcupsdnis_ports="default"
 
-server_cups_ports="tcp/ipp"
+server_cups_ports="tcp/631"
 client_cups_ports="default"
 
 server_cvspserver_ports="tcp/2401"
@@ -3494,7 +3494,7 @@ case "${arg}" in
 		else
 		
 		${CAT_CMD} <<"EOF"
-$Id: firehol.sh,v 1.140 2003/07/20 21:46:41 ktsaou Exp $
+$Id: firehol.sh,v 1.141 2003/07/20 21:48:01 ktsaou Exp $
 (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 FireHOL is distributed under GPL.
 
@@ -3680,7 +3680,7 @@ then
 	
 	${CAT_CMD} <<"EOF"
 
-$Id: firehol.sh,v 1.140 2003/07/20 21:46:41 ktsaou Exp $
+$Id: firehol.sh,v 1.141 2003/07/20 21:48:01 ktsaou Exp $
 (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 FireHOL is distributed under GPL.
 Home Page: http://firehol.sourceforge.net
@@ -3975,7 +3975,7 @@ then
 	
 	${CAT_CMD} >&2 <<"EOF"
 
-$Id: firehol.sh,v 1.140 2003/07/20 21:46:41 ktsaou Exp $
+$Id: firehol.sh,v 1.141 2003/07/20 21:48:01 ktsaou Exp $
 (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 FireHOL is distributed under GPL.
 Home Page: http://firehol.sourceforge.net
@@ -4068,7 +4068,7 @@ EOF
 	echo "# "
 
 	${CAT_CMD} <<"EOF"
-# $Id: firehol.sh,v 1.140 2003/07/20 21:46:41 ktsaou Exp $
+# $Id: firehol.sh,v 1.141 2003/07/20 21:48:01 ktsaou Exp $
 # (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 # FireHOL is distributed under GPL.
 # Home Page: http://firehol.sourceforge.net
