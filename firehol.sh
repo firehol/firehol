74d99237a1d1dd88f89638b6fe9ee5a8ab663abc
ktsaou <ktsaou>
2004-03-03 22:19:15 +0000

    Added:
    --tcp-flags SYN,RST,PSH,ACK,URG SYN,RST,PSH,URG
    
    to the list of malformed packets.

--- firehol.sh
+++ firehol.sh
@@ -10,7 +10,7 @@
 #
 # config: /etc/firehol/firehol.conf
 #
-# $Id: firehol.sh,v 1.179 2004/03/03 21:24:41 ktsaou Exp $
+# $Id: firehol.sh,v 1.180 2004/03/03 22:19:15 ktsaou Exp $
 #
 
 # Remember who you are.
@@ -1854,6 +1854,7 @@ protection() {
 				rule in chain "${in}_${work_name}" action "${mychain}"   proto tcp custom "--tcp-flags SYN,RST SYN,RST"			|| return 1
 				rule in chain "${in}_${work_name}" action "${mychain}"   proto tcp custom "--tcp-flags ALL     SYN,RST,ACK,FIN,URG"	|| return 1
 				rule in chain "${in}_${work_name}" action "${mychain}"   proto tcp custom "--tcp-flags ALL     FIN,URG,PSH"		|| return 1
+				rule in chain "${in}_${work_name}" action "${mychain}"   proto tcp custom "--tcp-flags SYN,RST,PSH,ACK,URG SYN,RST,PSH,URG"	|| return 1
 				
 				rule in chain "${mychain}" loglimit "MALFORMED BAD" action drop								|| return 1
 				;;
@@ -4026,7 +4027,7 @@ case "${arg}" in
 		else
 		
 		${CAT_CMD} <<EOF
-$Id: firehol.sh,v 1.179 2004/03/03 21:24:41 ktsaou Exp $
+$Id: firehol.sh,v 1.180 2004/03/03 22:19:15 ktsaou Exp $
 (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 FireHOL is distributed under GPL.
 
@@ -4212,7 +4213,7 @@ then
 	
 	${CAT_CMD} <<EOF
 
-$Id: firehol.sh,v 1.179 2004/03/03 21:24:41 ktsaou Exp $
+$Id: firehol.sh,v 1.180 2004/03/03 22:19:15 ktsaou Exp $
 (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 FireHOL is distributed under GPL.
 Home Page: http://firehol.sourceforge.net
@@ -4507,7 +4508,7 @@ then
 	
 	${CAT_CMD} >&2 <<EOF
 
-$Id: firehol.sh,v 1.179 2004/03/03 21:24:41 ktsaou Exp $
+$Id: firehol.sh,v 1.180 2004/03/03 22:19:15 ktsaou Exp $
 (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 FireHOL is distributed under GPL.
 Home Page: http://firehol.sourceforge.net
@@ -4590,7 +4591,7 @@ EOF
 	echo "# "
 
 	${CAT_CMD} <<EOF
-# $Id: firehol.sh,v 1.179 2004/03/03 21:24:41 ktsaou Exp $
+# $Id: firehol.sh,v 1.180 2004/03/03 22:19:15 ktsaou Exp $
 # (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 # FireHOL is distributed under GPL.
 # Home Page: http://firehol.sourceforge.net
