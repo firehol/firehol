bf26d800ce77ae91004b4fd68b339b95a0b92220
ktsaou <ktsaou>
2004-04-21 22:39:40 +0000

    FireHOL now checks that /var/lock/subsys (the dir RH uses for startup services lock files) exists before using it.

--- firehol.sh
+++ firehol.sh
@@ -10,7 +10,7 @@
 #
 # config: /etc/firehol/firehol.conf
 #
-# $Id: firehol.sh,v 1.186 2004/04/21 22:23:10 ktsaou Exp $
+# $Id: firehol.sh,v 1.187 2004/04/21 22:39:40 ktsaou Exp $
 #
 
 # Remember who you are.
@@ -4101,7 +4101,7 @@ case "${arg}" in
 		else
 		
 		${CAT_CMD} <<EOF
-$Id: firehol.sh,v 1.186 2004/04/21 22:23:10 ktsaou Exp $
+$Id: firehol.sh,v 1.187 2004/04/21 22:39:40 ktsaou Exp $
 (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 FireHOL is distributed under GPL.
 
@@ -4287,7 +4287,7 @@ then
 	
 	${CAT_CMD} <<EOF
 
-$Id: firehol.sh,v 1.186 2004/04/21 22:23:10 ktsaou Exp $
+$Id: firehol.sh,v 1.187 2004/04/21 22:39:40 ktsaou Exp $
 (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 FireHOL is distributed under GPL.
 Home Page: http://firehol.sourceforge.net
@@ -4581,7 +4581,7 @@ then
 	
 	${CAT_CMD} >&2 <<EOF
 
-$Id: firehol.sh,v 1.186 2004/04/21 22:23:10 ktsaou Exp $
+$Id: firehol.sh,v 1.187 2004/04/21 22:39:40 ktsaou Exp $
 (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 FireHOL is distributed under GPL.
 Home Page: http://firehol.sourceforge.net
@@ -4664,7 +4664,7 @@ EOF
 	echo "# "
 
 	${CAT_CMD} <<EOF
-# $Id: firehol.sh,v 1.186 2004/04/21 22:23:10 ktsaou Exp $
+# $Id: firehol.sh,v 1.187 2004/04/21 22:39:40 ktsaou Exp $
 # (C) Copyright 2003, Costa Tsaousis <costa@tsaousis.gr>
 # FireHOL is distributed under GPL.
 # Home Page: http://firehol.sourceforge.net
@@ -5298,8 +5298,13 @@ fi
 # Remove the saved firewall, so that the trap will not restore it.
 ${RM_CMD} -f "${FIREHOL_SAVED}"
 
-${TOUCH_CMD} /var/lock/subsys/iptables
-${TOUCH_CMD} /var/lock/subsys/firehol
+# RedHat startup service locking.
+if [ -d /var/lock/subsys ]
+then
+	${TOUCH_CMD} /var/lock/subsys/iptables
+	${TOUCH_CMD} /var/lock/subsys/firehol
+fi
+
 
 # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
