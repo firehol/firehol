004ac4675a12139c3e90ba43eb4c4688a97a8eca
Phil Whineray <phil@sanewall.org>
2013-11-10 21:01:03 +0000

    Tidy up regressions and output

--- regression/tools/run-suite.sh
+++ regression/tools/run-suite.sh
@@ -133,50 +133,61 @@ do
   fi
 done
 
-mkdir -p $outdir/ipv6 || myexit
-mkdir -p $outdir/ipv4-no-nat || myexit
-
 iptables-save > $MYTMP/save || myexit
 ip6tables-save > $MYTMP/save6 || myexit
 
-sort -u $MYTMP/list > $MYTMP/list.srt
+sort -u $MYTMP/list | sed -e 's;tests/;;' > $MYTMP/list.srt
 mv $MYTMP/list.srt $MYTMP/list
+
 while read testfile
 do
-  i=`echo $testfile | sed -e 's;/;-;g' -e s'/.conf$//'`
-  cfgfile="$outdir/$i.conf"
-  logfile="$outdir/$i.log"
-  v4out="$outdir/$i.out"
-  v6out="$outdir/ipv6/$i.out"
-  v4aud="$outdir/$i.aud"
-  v6aud="$outdir/ipv6/$i.aud"
-  v4nnout="$outdir/ipv4-no-nat/$i.out"
-  echo ""
+  i=$(echo $testfile | sed -e s'/.conf$//')
+  d=$(dirname $i)
+
+  mkdir -p $outdir/tests/$d || myexit
+  mkdir -p $outdir/ipv4/$d || myexit
+  mkdir -p $outdir/ipv6/$d || myexit
+  mkdir -p $outdir/ipv4-no-nat/$d || myexit
+done < $MYTMP/list
+
+while read testfile
+do
+  i=$(echo $testfile | sed -e s'/.conf$//')
+  d=$(dirname $i)
+  f=$(basename $i)
+  cfgfile="$outdir/tests/$d/$f.conf"
+  logfile="$outdir/tests/$d/$f.log"
+  v4out="$outdir/ipv4/$d/$f.out"
+  v6out="$outdir/ipv6/$d/$f.out"
+  v4nnout="$outdir/ipv4-no-nat/$d/$f.out"
+  v4aud="$outdir/ipv4/$d/$f.aud"
+  v6aud="$outdir/ipv6/$d/$f.aud"
+  v4nnaud="$outdir/ipv4-no-nat/$d/$f.aud"
   echo "  Running $cfgfile"
-  if grep -q "^====" "$here/$testfile"
+  if grep -q "^====" "$here/tests/$testfile"
   then
     audit="Y"
-    sigsfile="`echo $testfile | sed -e 's/.conf/.sigs/'`"
-    gpg --verify "$here/$sigsfile" "$here/$testfile"
-    sed -e '/^====/,$d' "$here/$testfile" > "$cfgfile"
+    sigsfile=$(echo $testfile | sed -e 's/.conf$/.sigs/')
+    test -f "$here/tests/$sigsfile" && gpg --verify "$here/tests/$sigsfile" "$here/tests/$testfile"
+    sed -e '/^====/,$d' "$here/tests/$testfile" > "$cfgfile"
     sed -e '1,/^==== IPv4 AUDITED O/d' \
-        -e '/^==== IPv4 AUDITED END/,$d' "$here/$testfile" > "$v4aud"
+        -e '/^==== IPv4 AUDITED END/,$d' "$here/tests/$testfile" \
+       | tools/reorg-save > "$v4aud"
+    tools/reorg-save -n "$v4aud" > "$v4nnaud"
     if grep -q IP6TABLES_CMD "$prog"
     then
       audit6="Y"
       sed -e '1,/^==== IPv6 AUDITED O/d' \
-          -e '/^==== IPv6 AUDITED END/,$d' "$here/$testfile" > "$v6aud"
+          -e '/^==== IPv6 AUDITED END/,$d' "$here/tests/$testfile" \
+       | tools/reorg-save > "$v6aud"
     else
       audit6=""
     fi
   else
     audit=""
     audit6=""
-    cp "$here/$testfile" "$cfgfile"
+    cp "$here/tests/$testfile" "$cfgfile"
   fi
-  echo "      log $logfile"
-  echo " v4result $v4out"
-  echo " v6result $v6out"
   clear_all
   $prog "$cfgfile" start >> "$logfile" 2>&1
   iptables-save > "$v4out".tmp
@@ -191,13 +202,26 @@ do
   tools/reorg-save -n "$v4out".tmp > "$v4nnout"
   tools/reorg-save "$v6out".tmp > "$v6out"
   rm -f "$v4out".tmp "$v6out".tmp
-  if [ "$audit" ]
-  then
-    cmp "$v4out" "$v4aud" || echo "Warning: output differs from audited version"
+  auditfail=0
+  if [ "$audit" ]; then
+    if ! cmp "$v4out" "$v4aud"; then
+      echo "Warning: ipv4 output differs from audited version"
+      echo " v4result $v4out"
+      echo "  v4audit $v4out"
+      auditfail=1
+    fi
   fi
-  if [ "$audit6" ]
-  then
-    cmp "$v6out" "$v6aud" || echo "Warning: output differs from audited version"
+  if [ "$audit6" ]; then
+    if ! cmp "$v6out" "$v6aud"; then
+      echo "Warning: ipv6 output differs from audited version"
+      echo " v6result $v6out"
+      echo "  v6audit $v6aud"
+      auditfail=1
+    fi
+  fi
+  if [ $auditfail -gt 0 ]; then
+    echo "      log $logfile"
+    echo ""
   fi
 done < $MYTMP/list
 
