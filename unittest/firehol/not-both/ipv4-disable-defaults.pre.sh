070430762dcea11964ec33d56bb249efc4b76278
Phil Whineray <phil@sanewall.org>
2015-10-26 22:05:43 +0000

    Fixup commands not using _CMD variables
    Also fix remaining problems around autodetection
    Both were exposed by the new unittest strategy

--- unittest/firehol/not-both/ipv4-disable-defaults.pre.sh
+++ unittest/firehol/not-both/ipv4-disable-defaults.pre.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Disable IPV4
-cat - > /etc/firehol/firehol-defaults.conf <<-END-DEFAULTS
+cat - >> /etc/firehol/firehol-defaults.conf <<-END-DEFAULTS
 ENABLE_IPV4=0
 END-DEFAULTS
--- unittest/firehol/not-both/ipv6-disable-defaults.pre.sh
+++ unittest/firehol/not-both/ipv6-disable-defaults.pre.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Disable IPV6
-cat - > /etc/firehol/firehol-defaults.conf <<-END-DEFAULTS
+cat - >> /etc/firehol/firehol-defaults.conf <<-END-DEFAULTS
 ENABLE_IPV6=0
 END-DEFAULTS
