ea252883d838fc6d36e03d699e9d9dbe8b1d86cd
Philip Whineray <phil@firehol.org>
2015-11-08 17:28:16 +0000

    Add perl script to detect plain command usage
    Update scripts with the problems found
    In firehol, moved the iptables() and ipset() helpers to before they are
    
    used, since this is how the detection script learns they are not a problem.

---
+++ packaging/detect-cmd.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Data::Dumper;
+use File::Basename;
+
+if (@ARGV == 0) {
+  print "Usage: ./packaging/detect-cmd.pl configure.ac sbin/file.in ...\n";
+  print "\n";
+  print "Finds usages of commands which should be converted to \$COMMAND_CMD format\n";
+  exit 0;
+}
+
+my %commands = ();
+foreach my $undetectable (qw/grep egrep sed/) {
+  $commands{$undetectable} = 1;
+}
+
+my $acfile = shift @ARGV;
+open C, "<$acfile" or die "Unable to open $acfile";
+while (<C>) {
+  if (/AX_CHECK_PROG/ or /AX_NEED_PROG/) {
+    my @fields = split /[][]/;
+    $commands{$fields[3]} = 1;
+  }
+}
+
+my @commands = sort(keys(%commands));
+
+#print join(",", @commands), "\n";
+#exit 0;
+
+sub printit {
+  my $message = join('', @_);
+
+  print basename($ARGV), ":", $., ": ", $message;
+}
+
+my $case = 0;
+my $case_start = 0;
+while (<>) {
+  next if (/^[YN]\|/); # Skip command tables
+  next if (/^[[:space:]]*$/); # Skip blank lines
+  next if (/^[[:space:]]#/); # Skip pure comments for efficiency
+  next if (/`which .*head/); # Skip special case - initial command detection
+
+  if (/^[[:space:]]*case\b/) {
+    $case++;
+    $case_start = 1;
+    next;
+  }
+  if ($case > 0) {
+    if (/^[[:space:]]*;;/) {
+      $case_start = 1;
+      next;
+    }
+    $case-- if (/^[[:space:]]*esac\b/);
+  } else {
+    $case_start = 0;
+  }
+
+  my @present=();
+  foreach my $command (@commands) {
+    push @present, $command if /\b$command\b/;
+  }
+
+  foreach my $present (@present) {
+    if (/#.*\b$present\b/) {
+      #printit "$present in a comment: $_";
+    } elsif (/\${?$present}?/) {
+      #printit "$present as a variable: $_";
+    } elsif (/`[^`]*\b$present\b/ and /[|&;`][[:space:]]*$present/) {
+      printit "$present in backtick substitution: $_";
+    } elsif (/(\$\(|\`)[^)]*\b$present\b/ and /[(|&;`][[:space:]]*$present/) {
+      printit "$present in substitution: $_";
+    } elsif ($case_start == 0 and
+             (/^[[:space:]]*$present$/ or /^[[:space:]]*$present\b[^=]/
+                                       or /[|&;][[:space:]]*$present\b/)) {
+      if (/^$present\(\)/) {
+        delete $commands{$present}; # Redefined as a function
+        @commands = sort(keys(%commands));
+      } else {
+        printit "$present used as a command: $_";
+      }
+    }
+  }
+  $case_start = 0 if ($case_start);
+}
